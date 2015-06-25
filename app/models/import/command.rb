f652bf171bccf9c288e47532cb0c441197b158b1
Sebastian Gaul <sgaul@milabent.com>
2015-06-25 13:11:52 +0200

    Implement Import::Command
    A simple Command class that executes all plans or a specific one.

---
+++ app/models/import/command.rb
@@ -0,0 +1,24 @@
+class Import::Command
+  def self.import_all
+    Plan.all.each do |plan|
+      import(plan)
+    end
+  end
+
+  def self.import(plan)
+    new(plan).execute
+  end
+
+  def initialize(plan)
+    @url = plan.url
+    @resource_class = plan.resource_class
+  end
+
+  def execute
+    @resource_class.with_import_url(@url).all.each do |resource|
+      if resource.valid?
+        resource._create_data_from_import
+      end
+    end
+  end
+end
