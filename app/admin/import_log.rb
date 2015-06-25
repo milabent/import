b0306bb1b4bcddf78526ca26c6ed26bb72db1655
Sebastian Gaul <sgaul@milabent.com>
2015-06-25 14:47:19 +0200

    Group Activeadmin menu

--- app/admin/import_log.rb
+++ app/admin/import_log.rb
@@ -1,12 +1,13 @@
 if defined?(ActiveAdmin)
-  ActiveAdmin.register ::Import::Plan do
-    menu priority: 98, parent: 'Import', label: 'Plans'
+  ActiveAdmin.register ::Import::Log do
+    menu priority: 99, parent: 'Import', label: 'Logs'
     permit_params :url, :resource_type, :interval
 
     index do
       selectable_column
       id_column
-      column :url
+      column :reason
+      column :plan
       column :created_at
       actions
     end
