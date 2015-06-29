d90365c148ad62a690cacd950394b429ea001631
Sebastian Gaul <sgaul@milabent.com>
2015-06-29 09:13:37 +0200

    Move admin definitions to namespace

--- lib/import/engine.rb
+++ lib/import/engine.rb
@@ -2,7 +2,8 @@ module Import
   class Engine < ::Rails::Engine
     initializer :import_engine do
       if defined?(ActiveAdmin)
-        ActiveAdmin.application.load_paths += Dir["#{config.root}/app/admin/**/"]
+        ActiveAdmin.application.load_paths += Dir["#{config.root}/app/models/**/"]
+        ActiveAdmin.application.load_paths += Dir["#{config.root}/app/admin/import/**/"]
       end
     end
   end
